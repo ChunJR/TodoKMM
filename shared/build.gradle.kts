ae936860d5ee44c06c1e65d8d0f4370ba7eafe55
O2F-IT\vitrung.trinh_dksh <vitrung.trinh@dksh.com>
2023-09-12 09:43:44 +0700

    fixed bug gradle. TODO: migrate ktor v1.x to v2.x

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,16 +4,17 @@ plugins {
     id("com.squareup.sqldelight")
 }
 
-version = "1.0.0"
-val sqldelightVersion = "1.5.3"
-val ktorVersion = "1.6.1"
-val kodeinVersion = "7.6.0"
-val logbackVersion = "1.0.0"
-val serializationVersion = "1.1.0"
-val coroutineVersion = "1.5.1-native-mt"
-
+@OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    android()
+    targetHierarchy.default()
+
+    android {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
 
     listOf(
         iosX64(),
@@ -25,59 +26,44 @@ kotlin {
         }
     }
 
+    val coroutinesVersion = "1.7.1"
+    val ktorVersion = "2.3.2"
+    val sqlDelightVersion = "1.5.5"
+    val dateTimeVersion = "0.4.0"
+    val kodeinVersion = "7.6.0"
+    val logbackVersion = "1.4.8"
+    val napierVersion = "1.5.0"
+    val serializationVersion = "1.5.1"
+    val coroutineVersion = "1.5.1-native-mt"
+
     sourceSets {
+        targetHierarchy.default()
+
         val commonMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
-                implementation("com.squareup.sqldelight:runtime:$sqldelightVersion")
-                implementation("org.kodein.di:kodein-di:$kodeinVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
                 implementation("io.ktor:ktor-client-core:$ktorVersion")
-                implementation("io.ktor:ktor-client-logging:$ktorVersion")
-                implementation("io.ktor:ktor-client-auth:$ktorVersion")
-                implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+                implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+                implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+                implementation("com.squareup.sqldelight:runtime:$sqlDelightVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:$dateTimeVersion")
+                implementation("org.kodein.di:kodein-di:7.6.0")
+                implementation("org.kodein.di:kodein-di-framework-android-x:7.6.0")
                 implementation("ch.qos.logback:logback-classic:$logbackVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serializationVersion")
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion") {
-                    version {
-                        strictly(coroutineVersion)
-                    }
-                }
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
             }
         }
         val androidMain by getting {
             dependencies {
-                implementation("com.squareup.sqldelight:android-driver:$sqldelightVersion")
                 implementation("io.ktor:ktor-client-android:$ktorVersion")
+                implementation("com.squareup.sqldelight:android-driver:$sqlDelightVersion")
             }
         }
-        val androidTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
+        val iosMain by getting {
+            // ...
             dependencies {
-                implementation("com.squareup.sqldelight:native-driver:$sqldelightVersion")
                 implementation("io.ktor:ktor-client-ios:$ktorVersion")
+                implementation("com.squareup.sqldelight:native-driver:$sqlDelightVersion")
             }
-
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val iosX64Test by getting
-        val iosArm64Test by getting
-        val iosSimulatorArm64Test by getting
-        val iosTest by creating {
-            dependsOn(commonTest)
-            iosX64Test.dependsOn(this)
-            iosArm64Test.dependsOn(this)
-            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
@@ -93,7 +79,6 @@ android {
     namespace = "com.example.todokmm"
     compileSdk = 33
     defaultConfig {
-        minSdk = 21
-        targetSdk = 33
+        minSdk = 24
     }
 }
